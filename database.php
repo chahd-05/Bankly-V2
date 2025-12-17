5f50358e2561a1c37197c3b6f8fdaff25aee0815
shahd <mazouzchahd12@gmail.com>
2025-12-17 10:18:40 +0100

    feat: create database of login

---
+++ database.php
@@ -0,0 +1,10 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "bankly";
+    $connect ="";
+    
+
+    $connect = mysqli_connect($server, $user, $password, $database, 3307);
+?>
